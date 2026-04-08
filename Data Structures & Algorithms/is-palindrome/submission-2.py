c167faa6cc9f5f7cfe8ee5b4c36fa947ea64b444
SManojna <saimanojnavelagala@gmail.com>
2026-04-08 15:04:03 -0500

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.py
@@ -0,0 +1,10 @@
+import re
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        s = re.sub(r'[^\w\n]','',s)
+        l = len(s)
+        mid = l//2
+        return s == s[::-1]
+
+        
